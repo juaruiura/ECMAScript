b668455185f9621199995fd7cad2207113a536a1
Juan Ruiz <ruizuranojuanmanuel@gmail.com>
2020-11-16 19:57:14 +0000

    let, const, multiline, spread operator and destructuring

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,4 @@
+//Defaults
 // Antes de ECMAScript6
 function newFunction(name, age, country) {
     var name = name || 'oscar';
@@ -10,6 +11,7 @@ function newFunction2(name = 'oscar', age = 32, country = "MX") {
     console.log(name, age, country);
 }
 
+//Concatenate
 // Antes
 newFunction2();
 newFunction2('Ricardo', 23, 'CO');
@@ -20,3 +22,45 @@ console.log(epicPhrase);
 // Después
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+//Multiline
+//Antes
+let lorem = "Quiero escribir una frase épica que podamos separar \n"
++ "otra frase épica que necesitamos.";
+console.log(lorem);
+//Después
+let lorem2 = `Esto es una frase épica que podamos separar
+ahora es otra frase épica.`;
+console.log(lorem2);
+
+//Destructuring
+let person = {
+    "name": "oscar",
+    "age" : 32,
+    "country" : "MX"
+}
+//Antes
+console.log(person.name, person.age);
+//Después
+let { name} = person;
+console.log(name);
+
+//Spread Operator
+let team1 = ["Oscar", "Julian", "Ricardo"];
+let team2 = ["Valeria", "Yesica", "Camila"];
+let education = ["David", ...team1, ...team2];
+console.log(education);
+
+//Scope: Let/Const
+{
+    var globalVar = "Global Var";
+}
+{
+    let globalLet = "Global Let"
+    console.log(`Aquí sí está definido ${globalLet}`);
+}
+console.log(globalVar);
+console.log(globalLet);
+const a = "a";
+//a = "b" Assigntment to constant variable Error;
+console.log(a);
